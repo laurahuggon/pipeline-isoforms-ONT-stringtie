e3cefeb5d3760ff0824d678c033e2da5529977b8
laurahuggon <laurahuggon@gmail.com>
2025-10-11 20:45:12 +0100

    Update barcode13

--- run_pipeline-isoforms-ont-stringtie.sh
+++ run_pipeline-isoforms-ont-stringtie.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -l
-#SBATCH --job-name=ont_barcode12_annotation
+#SBATCH --job-name=ont_barcode13_annotation
 #SBATCH --output=/scratch/prj/bcn_synaptopathy/%j.out
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
@@ -11,7 +11,7 @@
 
 # Check number of files
 ls /scratch/prj/bcn_synaptopathy/input_fastq/*.fastq.gz | wc -l
-ls /scratch/prj/bcn_synaptopathy/input_fastq/*barcode12*.fastq.gz | wc -l
+ls /scratch/prj/bcn_synaptopathy/input_fastq/*barcode13*.fastq.gz | wc -l
 
 # Load anaconda
 module load anaconda3/2022.10-gcc-13.2.0
